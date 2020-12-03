baf28d8638069f3e84b13cf9e208a49f5b9f65e5
yuuuniii49 <0409yoon@sookmyung.ac.kr>
2020-12-03 15:27:52 +0900

    03

--- main.c
+++ main.c
@@ -2,32 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 struct Book {
 	int number;
 	char title[15];
 };
 
 int main(void) {
-	struct Book *p;
+	int i;
+	struct Book **bookshelf;
 	
-	// Book 구조체 2개 동적할당 
-	p = (struct Book*) malloc (2*sizeof(struct Book));
-	
-	if (p == NULL) {
-		printf("메모리 할당 오류\n");
-		exit(1);
+	bookshelf = (struct Book**)malloc(3 * sizeof (struct Book*));
+	for (i=0;i<3;i++) {
+		bookshelf[i] = (struct Book*)malloc(10 * sizeof (struct Book));
 	}
+		
+	bookshelf[1][3].number = 5;
+	strcpy(bookshelf[1][3].title, "C++ Programming");
 	
-	p->number = 1;
-	strcpy(p->title, "C Programming");
-	
-	(p+1)->number = 2;
-	strcpy((p+1)->title, "Electronics");
-	
-	printf("%d : %s\n", p->number, p->title);
-	printf("%d : %s\n", (p+1)->number, (p+1)->title);
-	
-	free(p);
-	
+	(bookshelf[2]+4)->number = 3;
+	strcpy((bookshelf[2]+4)->title, "Communications Theory");
+
+	printf("book (1,3) : %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
+	printf("book (2,4) : %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title);
+
+	for (i=0;i<3;i++) {
+		free(bookshelf[i]);
+	}
+		
+	free(bookshelf);
+
 	return 0;
 }
